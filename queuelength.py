939062692718a3814642eaa431be918884fbea31
Matthew Heffernan <mrheffernan@email.wm.edu>
2019-06-25 11:40:47 -0400

    Adding command line file length checker as a check

--- queuelength.py
+++ queuelength.py
@@ -2,5 +2,7 @@
 
 import os
 username = ''
-print('Number of jobs running: ',os.popen('squeue -u %s').read().count('\n') % (username)) # don't need to add 1 for the last line with no \n since the first line returned isn't a job, therefore compensating for this
+print('Number of jobs running and in queue: ',int(os.popen('squeue -u %s').read().count('\n')-1) % (username)) 
+
+print('Checking by another means: ',os.system('squeue -u heff1693 | wc -l'))
 
